392a621c17dde1e55e56b0931163fe22dc724a27
Nithin <nithin@Nithins-MacBook-Air.local>
2025-02-05 09:25:48 -0600

    Added floatingdock to the backgroundlines and completed Homepage setup

--- src/components/backgroundlines.js
+++ src/components/backgroundlines.js
@@ -1,9 +1,14 @@
 import React from "react";
 import { BackgroundLines } from "@/components/ui/background-lines";
+import Image from "next/image";
+import { FloatingDockDemo } from "../components/FloatingDock.js"
 
 export default function BackgroundLinesDemo() {
   return (
-    (<BackgroundLines className="flex items-center justify-center w-full flex-col px-4">
+    (
+    <div className="flex items-center justify-center w-full flex-col px-4">
+    <BackgroundLines className="flex flex-col items-center text-center w-full">
+    <Image className= "relative z-20 border-4 border-white rounded-full mt-20" src="/myimage.jpeg" alt="x" width={300} height={300}  />
       <h2
         className="bg-clip-text text-transparent text-center bg-gradient-to-b from-neutral-900 to-neutral-700 dark:from-neutral-600 dark:to-white text-2xl md:text-4xl lg:text-7xl font-sans py-2 md:py-10 relative z-20 font-bold tracking-tight">
         Sai Nithin Reddy
@@ -12,6 +17,11 @@ export default function BackgroundLinesDemo() {
         className="max-w-xl mx-auto text-sm md:text-lg text-neutral-700 dark:text-neutral-400 text-center">
         Welcome to my portfolio <br /> Data Engineer, Software Developer
       </p>
-    </BackgroundLines>)
+      <div className="mt-10">
+      <FloatingDockDemo/>
+      </div>
+    </BackgroundLines>
+    </div>
+    )
   );
 }
--- src/pages/home.js
+++ src/pages/home.js
@@ -2,12 +2,18 @@
 import  NavbarDemo  from "../components/Navbar.js"; // Adjust the path if needed
 import  BackgroundLinesDemo  from "../components/backgroundlines.js";
 import { FloatingNavDemo } from "../components/FloatingNav.js"
+import { FloatingDockDemo } from "../components/FloatingDock.js"
+import { HeroScrollDemo } from "../components/Container.js"
+import { TimelineDemo } from "../components/Timeline.js";
 
 export default function Home() {
     return(
         <div>
             <NavbarDemo/>
             <BackgroundLinesDemo/>
+            
+            <TimelineDemo/>
+            <HeroScrollDemo/>
         </div>
 
     )
