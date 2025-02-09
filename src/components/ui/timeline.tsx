177b4da7a03b276a0db7d557de173752bcdcef1a
Nithin <nithin@Mac.home>
2025-02-08 18:12:57 -0600

    Updated 1st build

--- src/components/ui/timeline.tsx
+++ src/components/ui/timeline.tsx
@@ -37,7 +37,7 @@ export const Timeline = ({ data }: { data: TimelineEntry[] }) => {
       className="w-full bg-white dark:bg-neutral-950 font-sans md:px-10"
       ref={containerRef}
     >
-      <div className="max-w-7xl mx-auto py-20 px-4 md:px-8 lg:px-10 mt-[-16rem]">
+      <div className="max-w-7xl mx-auto py-20 px-4 md:px-8 lg:px-10 mt-[-16rem] ">
         <h2 className="text-lg md:text-4xl mb-4 text-black dark:text-white max-w-4xl">
           MY JOURNEY
         </h2>
@@ -50,7 +50,7 @@ export const Timeline = ({ data }: { data: TimelineEntry[] }) => {
         {data.map((item, index) => (
           <div
             key={index}
-            className="flex justify-start pt-10 md:pt-40 md:gap-10"
+            className="flex justify-start pt-20 md:pt-40 md:gap-10"
           >
             <div className="sticky flex flex-col md:flex-row z-40 items-center top-40 mt-[-5rem] self-start max-w-xs lg:max-w-sm md:w-full ">
               <div className="h-10 absolute left-3 md:left-3 w-10 rounded-full bg-white dark:bg-black flex items-center justify-center">
